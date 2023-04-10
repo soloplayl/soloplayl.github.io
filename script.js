591f7af57e92b2a04bdec2bdd8a34455832d5125
soloplayl <96019888+soloplayl@users.noreply.github.com>
2023-04-11 00:16:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
 const input = document.getElementById('input');
 const submit = document.getElementById('submit');
 const response = document.getElementById('response');
-const apiKey = 'YOUR_API_KEY_HERE';
+const apiKey = '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';
 
 submit.addEventListener('click', async () => {
 const question = input.value;
