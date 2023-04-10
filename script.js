cb4453207f57de54757bbf42f23781a521a400dd
soloplayl <96019888+soloplayl@users.noreply.github.com>
2023-04-11 00:16:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
 
 const input = document.getElementById('input');
 const submit = document.getElementById('submit');
 const response = document.getElementById('response');
-const apiKey = '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';
+const apiKey = 'YOUR_API_KEY_HERE';
 
 submit.addEventListener('click', async () => {
-  const question = input.value;
-  if (question) {
-    const data = {
-      prompt: question,
-      max_tokens: 100,
-      temperature: 0.5,
-    };
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiKey,
-      },
-      body: JSON.stringify(data),
-    });
-    const json = await response.json();
-    response.innerText = json.choices[0].text;
-  }
+const question = input.value;
+if (question) {
+const data = {
+prompt: question,
+max_tokens: 100,
+temperature: 0.5,
+engine: "davinci"
+};
+const response = await fetch(apiUrl, {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+'Authorization': 'Bearer ' + apiKey,
+},
+body: JSON.stringify(data),
+});
+const json = await response.json();
+response.innerText = json.choices[0].text;
+}
 });
